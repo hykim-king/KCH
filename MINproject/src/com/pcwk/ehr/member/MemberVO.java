7caee73df205deeb1890ba5796cb7500e96390ed
강채린 <112448910+QWOFTQ@users.noreply.github.com>
2024-10-25 11:53:11 +0900

    Add files via upload

---
+++ MINproject/src/com/pcwk/ehr/member/MemberVO.java
@@ -0,0 +1,5 @@
+package com.pcwk.ehr.member;
+
+public class MemberVO {
+
+}
---
+++ MINproject/src/module-info.java
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MINIproject {
+	requires com.google.gson;
+	requires org.jsoup;
+	requires org.apache.logging.log4j;
+	requires org.apache.logging.log4j.core;
+	requires org.json;
+	requires JMember;
+}
